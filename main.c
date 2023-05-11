38666de21235d4cd3cb8a676c1381a0837660068
Sofía <ravennasofia03@gmail.com>
2023-05-10 21:55:30 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,14 @@
+/*4. Utilizando funciones crear un programa que represente, a través de una estructura,
+puntos en tres dimensiones. Se pide diseñar funciones para resolver las siguientes
+operaciones:
+a. Suma de dos puntos.
+b. Distancia al origen.
+c. Producto vectorial.*/
+
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
